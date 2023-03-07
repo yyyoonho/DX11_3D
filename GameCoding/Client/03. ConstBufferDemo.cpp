30088212ac44b9f69b269035312843165c61b60b
yyyoonho <parn997@naver.com>
2023-03-08 02:03:28 +0900

    feat: ConstantBuffer

---
+++ GameCoding/Client/03. ConstBufferDemo.cpp
@@ -0,0 +1,59 @@
+#include "pch.h"
+#include "03. ConstBufferDemo.h"
+#include "geometryHelper.h"
+
+void ConstBufferDemo::Init()
+{
+	_shader = make_shared<Shader>(L"03. ConstBuffer.fx");
+
+	_geometry = make_shared<Geometry<VertexColorData>>();
+	GeometryHelper::CreateQuad(_geometry, Color{ 1.f, 1.f, 0.f, 1.f });
+
+	_vertexBuffer = make_shared<VertexBuffer>();
+	_vertexBuffer->Create(_geometry->GetVertices());
+
+	_indexBuffer = make_shared<IndexBuffer>();
+	_indexBuffer->Create(_geometry->GetIndices());
+}
+
+void ConstBufferDemo::Update()
+{
+	float dt = TIME->GetDeltaTime();
+
+	if (INPUT->GetButton(KEY_TYPE::A))
+	{
+		_translation.x -= 3.f * dt;
+	}
+	else if (INPUT->GetButton(KEY_TYPE::D))
+	{
+		_translation.x += 3.f * dt;
+	}
+	else if (INPUT->GetButton(KEY_TYPE::W))
+	{
+		_translation.y += 3.f * dt;
+	}
+	else if (INPUT->GetButton(KEY_TYPE::S))
+	{
+		_translation.y -= 3.f * dt;
+	}
+
+	// SRT (현재 S,R은 Identity 상태에서 변화없음으로 잠깐 생략하자.)
+	_world = Matrix::CreateTranslation(_translation);
+
+
+}
+
+void ConstBufferDemo::Render()
+{
+	_shader->GetMatrix("World")->SetMatrix((float*)&_world);
+	_shader->GetMatrix("View")->SetMatrix((float*)&_view);
+	_shader->GetMatrix("Projection")->SetMatrix((float*)&_world);
+
+	uint32 stride = _vertexBuffer->GetStride();
+	uint32 offset = _vertexBuffer->GetOffset();
+
+	DC->IASetVertexBuffers(0, 1, _vertexBuffer->GetComPtr().GetAddressOf(), &stride, &offset);
+	DC->IASetIndexBuffer(_indexBuffer->GetComPtr().Get(), DXGI_FORMAT_R32_UINT, 0);
+	
+	_shader->DrawIndexed(0, 0, _indexBuffer->GetCount(), 0, 0);
+}
--- GameCoding/Client/Main.cpp
+++ GameCoding/Client/Main.cpp
@@ -3,6 +3,7 @@
 #include "Engine/Game.h"
 #include "01. TriangleDemo.h"
 #include "02. QuadDemo.h"
+#include "03. ConstBufferDemo.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
@@ -14,7 +15,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	desc.width = 800;
 	desc.height = 600;
 	desc.clearColor = Color(0.5f, 0.5f, 0.5f, 0.5f);
-	desc.app = make_shared<QuadDemo>();
+	desc.app = make_shared<ConstBufferDemo>();
 
 	GAME->Run(desc);
 
